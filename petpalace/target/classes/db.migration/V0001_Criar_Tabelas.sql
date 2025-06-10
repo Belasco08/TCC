db2b61fdc2c4f4593181e4c20b848fb13b304d31
Belasco <162700278+Belasco08@users.noreply.github.com>
2025-06-10 12:11:23 -0300

    Add files via upload

--- petpalace/target/classes/db.migration/V0001_Criar_Tabelas.sql
+++ petpalace/target/classes/db.migration/V0001_Criar_Tabelas.sql
@@ -1,74 +1,91 @@
-CREATE TABLE usuarios (
-    id bigint PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    senha VARCHAR(255) NOT NULL,
-    telefone VARCHAR(20),
-    tipo_usuario ENUM('cliente', 'anfitriao') NOT NULL
+create table usuarios (
+    id bigint primary key auto_increment,
+    nome varchar(100) not null,
+    email varchar(100) not null,
+    senha varchar(255) not null,
+    telefone varchar(20),
+    tipo_usuario enum('cliente', 'anfitriao') not null
 );
-CREATE TABLE enderecos (
-    id bigint not null PRIMARY KEY AUTO_INCREMENT,
-    rua VARCHAR(100) NOT NULL,
-    numero VARCHAR(10) NOT NULL,
-    complemento VARCHAR(100),
-    bairro VARCHAR(100) NOT NULL,
-    cep VARCHAR(15) NOT NULL,
-    cidade_id bigint NOT NULL
+create table enderecos (
+    id bigint not null primary key auto_increment,
+    rua varchar(100) not null,
+    numero varchar(10) not null,
+    complemento varchar(100),
+    bairro varchar(100) not null,
+    cep varchar(15) not null,
+    cidade_id bigint not null
 );
-CREATE TABLE cidades (
-    id bigint PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100) NOT NULL,
-    estado_id bigint NOT NULL
+create table cidades (
+    id bigint primary key auto_increment,
+    nome varchar(100) not null,
+    estado_id bigint not null
 );
-CREATE TABLE estados (
-    id bigint PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100) NOT NULL,
-    sigla VARCHAR(2) NOT NULL,
-    pais_id bigint NOT NULL
+create table estados (
+    id bigint primary key auto_increment,
+    nome varchar(100) not null,
+    sigla varchar(2) not null,
+    pais_id bigint not null
 );
-CREATE TABLE paises (
-    id bigint PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100) NOT NULL
+create table paises (
+    id bigint primary key auto_increment,
+    nome varchar(100) not null
 );
-CREATE TABLE animais (
-    id bigint PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100) NOT NULL,
-    especie ENUM('cachorro', 'gato') NOT NULL,
-    raca VARCHAR(100),
-    idade INT,
-    peso DECIMAL(5,2),
-    observacoes TEXT,
-    id_usuario bigint NOT NULL
+create table animais (
+    id bigint primary key auto_increment,
+    nome varchar(100) not null,
+    especie enum('cachorro', 'gato') not null,
+    raca varchar(100),
+    idade int,
+    peso decimal(5,2),
+    observacoes text,
+    id_usuario bigint not null
 );
-CREATE TABLE anuncios (
-    id bigint PRIMARY KEY AUTO_INCREMENT,
-    id_anfitriao bigint NOT NULL,
-    titulo VARCHAR(100) NOT NULL,
-    descricao TEXT,
-    preco_diaria DECIMAL(10,2) NOT NULL,
-    aceita_gatos BOOLEAN DEFAULT FALSE,
-    aceita_caes BOOLEAN DEFAULT FALSE,
-    vagas_disponiveis INT NOT NULL,
-    id_endereco bigint NOT NULL,
-    data_criacao DATE
+create table anuncios (
+    id bigint primary key auto_increment,
+    id_anfitriao bigint not null,
+    titulo varchar(100) not null,
+    descricao text,
+    preco_diaria decimal(10,2) not null,
+    aceita_gatos boolean default false,
+    aceita_caes boolean default false,
+    vagas_disponiveis int not null,
+    id_endereco bigint not null,
+    data_criacao date
 );
-CREATE TABLE reservas (
-    id bigint PRIMARY KEY AUTO_INCREMENT,
-    id_cliente bigint NOT NULL,
-    id_anuncio bigint NOT NULL,
-    id_animal bigint NOT NULL,
-    data_inicio DATE NOT NULL,
-    data_fim DATE NOT NULL,
-    status ENUM('pendente', 'confirmada', 'cancelada') NOT NULL,
-    data_reserva DATE
+create table reservas (
+    id bigint primary key auto_increment,
+    id_cliente bigint not null,
+    id_anuncio bigint not null,
+    id_animal bigint not null,
+    data_inicio date not null,
+    data_fim date not null,
+    status enum('pendente', 'confirmada', 'cancelada') not null,
+    data_reserva date
 );
-CREATE TABLE avaliacoes (
-    id bigint PRIMARY KEY AUTO_INCREMENT,
-    id_reserva bigint NOT NULL,
-    nota INT CHECK (nota >= 1 AND nota <= 5),
-    comentario TEXT,
-    data_avaliacao DATE
+create table avaliacoes (
+    id bigint primary key auto_increment,
+    id_reserva bigint not null,
+    nota int check (nota >= 1 and nota <= 5),
+    comentario text,
+    data_avaliacao date
 );
+create table servico (
+    id bigint auto_increment primary key,
+    descricao varchar(255),
+    nome varchar(100) not null,
+    preco decimal(10,2) not null
+);
+
+create table agendamento (
+    id bigint auto_increment primary key,
+    data date not null,
+    horario time not null,
+    observacoes varchar(255),
+    usuario_id bigint not null,
+    pet_id bigint not null,
+    servico_id bigint not null,
+);
+
 alter table enderecos add constraint fk_endereco_cidade foreign key (cidade_id) references cidades(id);
 alter table cidades add constraint fk_cidade_estado foreign key (estado_id) references estados(id);
 alter table estados add constraint fk_estado_pais foreign key (pais_id) references paises(id);
@@ -78,3 +95,6 @@ alter table anuncios add constraint fk_anuncio_endereco foreign key (id_endereco
 alter table reservas add constraint fk_reserva_cliente foreign key (id_cliente) references usuarios(id);
 alter table reservas add constraint fk_reserva_anuncio foreign key (id_anuncio) references anuncios(id);
 alter table reservas add constraint fk_reserva_animal foreign key (id_animal) references animais(id);
+alter table agendamento add constraint fk_agendamento_usuario foreign key (usuario_id) references usuario(id);
+alter table agendamento add constraint fk_agendamento_pet foreign key (pet_id) references pet(id);
+alter table agendamento add constraint fk_agendamento_servico foreign key (servico_id) references servico(id);
